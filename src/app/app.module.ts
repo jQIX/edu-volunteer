9b1cd2fe83a2395f5ec6457a1c729cbef97e41ab
Xiaojie Qi <qi.xiaojie@gmail.com>
2018-10-22 21:46:40 -0700

    add navigation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { MyMaterialModule } from './my-material.module';
+import { MyMaterialModule } from './material/my-material.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
